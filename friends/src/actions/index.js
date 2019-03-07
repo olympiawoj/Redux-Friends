177a7494be8c6afd0b0c1dbed030bd5aa827e294
olympia <owojcik7@gmail.com>
2019-03-07 18:03:42 -0500

    Set item in local storage to token - user can now log in to see protected stuff

--- friends/src/actions/index.js
+++ friends/src/actions/index.js
@@ -5,7 +5,10 @@ export const LOGIN_START = "LOGIN_START";
 export const login = creds => dispatch => {
   dispatch({ type: LOGIN_START });
   return axios
-    .post("/api/login", creds)
-    .then(res => console.log(res))
+    .post("http://localhost:5000/api/login", creds)
+    .then(res => {
+      localStorage.setItem("token", res.data.payload);
+      dispatch({ type: "LOGIN_SUCCESS", payload: res.data.payload });
+    })
     .catch(err => console.log("YOU HAVE AN ERROR"));
 };
